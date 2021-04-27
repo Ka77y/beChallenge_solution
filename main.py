c039cb9e9d9183c195bc342dd987ce29d47a18ef
Katherine Vivanco <geomitaviva@hotmail.com>
2021-04-27 15:37:51 -0500

    Change port

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 from api import *
 from models import *
+import os
+port = int(os.environ.get("PORT", 5000))
 
 if __name__ == '__main__':
     db.connect()
     db.create_tables([User])
     app.run(host='0.0.0.0',
             debug=True,
-            port='$PORT')
+            port=port)
