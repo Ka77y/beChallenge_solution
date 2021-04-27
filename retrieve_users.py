d676a1d2385298907d34ff26d315ba6fefde1393
Katherine Vivanco <geomitaviva@hotmail.com>
2021-04-27 12:29:14 -0500

    Delete log result

--- retrieve_users.py
+++ retrieve_users.py
@@ -5,7 +5,6 @@ from playhouse.shortcuts import model_to_dict
 def retrieveUsers(username, per_page, page):
     response = []
     user_1 = User.select().where(User.username.contains(username)).paginate(page, per_page)
-    print(user_1)
     for user_response in user_1:
         response.append(model_to_dict(user_response))
     return str(response)
